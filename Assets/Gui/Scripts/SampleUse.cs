1dbe2b4e0f990307c0f5b34a8b5d83b7c8c2f238
yamada <the.first.victim0000@gmail.com>
2019-06-16 00:44:57 +0900

    Gui部分のテキストを追加

---
+++ Assets/Gui/Scripts/SampleUse.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class test : MonoBehaviour
+{
+    GameObject gui; //guiのオブジェクト
+    GuiTextMessage script; //GuiTextMessageスクリプトが入る変数
+
+    // Use this for initialization
+    void Start()
+    {
+        gui = GameObject.Find("GUI"); // GUIオブジェクトの検索
+        script = gui.GetComponent<GuiTextMessage>(); // GuiTextMessageスクリプトをget
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //電気つけたときとか。
+        if (Input.GetKey(KeyCode.Space))
+        {
+            // 3秒間テキスト表示
+            script.setTextWithTime("少女「デンキ ツイタ」", 3);
+        }
+    }
+}
