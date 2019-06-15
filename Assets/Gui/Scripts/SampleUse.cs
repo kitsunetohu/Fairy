fd2c3989cd5b929d0d8f3149f16b19067e58520d
yamada <the.first.victim0000@gmail.com>
2019-06-16 01:09:16 +0900

    バグ修正。sampleuseに名称変更。

--- Assets/Gui/Scripts/SampleUse.cs
+++ Assets/Gui/Scripts/SampleUse.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class test : MonoBehaviour
+public class SampleUse : MonoBehaviour
 {
     GameObject gui; //guiのオブジェクト
     GuiTextMessage script; //GuiTextMessageスクリプトが入る変数
